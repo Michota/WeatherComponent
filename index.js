cc336fcec5bfc366f5323f9029781a110588b0bb
Michota <dawidmichota@gmail.com>
2023-07-05 14:19:12 +0200

    test

---
+++ index.js
@@ -0,0 +1,1 @@
+import weather from "./js/weather";
---
+++ js/controller.js
@@ -0,0 +1,10 @@
+"use strict";
+import weather from "./weather.js";
+
+try {
+  const container = document.querySelector(".container");
+  // const component = weather.addComponent(container, [50.8690432, 20.64384]);
+  weather.start(container);
+} catch (err) {
+  console.log(err);
+}
