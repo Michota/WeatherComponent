88469696feced0813879892202a09e6d43c0f8fe
Michota <dawidmichota@gmail.com>
2023-07-05 13:54:05 +0200

    .

--- js/controller.js
+++ js/controller.js
@@ -4,7 +4,7 @@ import weather from "./weather.js";
 try {
   const container = document.querySelector(".container");
   // const component = weather.addComponent(container, [50.8690432, 20.64384]);
-  weather.start(container, "Kielce");
+  weather.start(container);
 } catch (err) {
   console.log(err);
 }
